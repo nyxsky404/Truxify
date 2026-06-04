da01e39d04f313ad0d75e03832526d53e9628ad6
25f1000058 <25f1000058@ds.study.iitm.ac.in>
2026-06-04 16:11:09 +0530

    Preliminary edits for saved addresses and payments integration

--- apps/customer/lib/main.dart
+++ apps/customer/lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'app.dart';
 
-void main() {
-  //Test
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: const String.fromEnvironment('SUPABASE_URL'),
+    anonKey: const String.fromEnvironment('SUPABASE_ANON_KEY'),
+  );
+
   runApp(const TruxifyApp());
 }
